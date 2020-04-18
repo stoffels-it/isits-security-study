87fa764a83379c0ca4adf6822c2c66be9ddbcac2
Sarah Stoffels <github@stoffels.it>
2020-04-19 00:54:50 +0200

    use whitespaces instead of leading zeros

--- introduction_cryptography/ciphertext_frequency_analysis.py
+++ introduction_cryptography/ciphertext_frequency_analysis.py
@@ -5,9 +5,9 @@ from subprocess import Popen, PIPE, STDOUT
 
 counts = list()
 digits = 0
-for c in ascii_lowercase:
+for char in ascii_lowercase:
     p = Popen(
-	"grep -ci "+c+" chiffrat.txt",
+	"grep -ci "+char+" chiffrat.txt",
 	shell=True,
 	stdin=PIPE,
 	stdout=PIPE,
@@ -15,21 +15,21 @@ for c in ascii_lowercase:
 	close_fds=True
     )
     count = p.stdout.read().decode('utf-8')
-    # get number of digits for leading zeros at later print
+    # get number of digits for leading whitespaces at later print
     if len(count) > digits:
         digits = len(count)
 
     # add as tuple to list
-    counts.append((c, int(count)))
+    counts.append((char, int(count)))
 
-allsum = sum(i for _, i in counts)
+allsum = sum(count for _, count in counts)
 one_percent = allsum / 100
 
 print("all:", allsum)
-for count in counts:
-    relative = round(count[1] / one_percent, 2)
+for char,count in counts:
+    relative = round(count / one_percent, 2)
     print(
-	"character:", count[0],
-	"absolute:", str(count[1]).zfill(digits),
+	"character:", char,
+	"absolute:", (str(count)).rjust(digits),
 	"relative:", relative
     )
