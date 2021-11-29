ef17744d898231f3774539f5fc843e8023a84988
Sarah Stoffels <github@stoffels.it>
2021-11-29 12:50:11 +0100

    Add basic script for sha1 hash determination

---
+++ network_security/determine_sha1_hash_unsalted.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# A hash is given and compared with hashes generated from a password
+# dictionary to determine the password. The hashes are without salt.
+
+import hashlib
+
+searchpw = '46256e1e9f868bce7e89cef99004c1600e17da2e'
+
+with open('common_passwords.txt') as file:
+    lines = file.read().splitlines()
+
+for line in lines:
+    h = hashlib.sha1(line.encode('utf-8')).hexdigest()
+    if h == searchpw:
+        print("password found: %s" % line)
